010df141c0a1dc10bfbb67d1747357748cd9ffc9
chanu <yuyutyanu@yahoo.co.jp>
2019-05-06 01:25:05 +0900

    add: log

---
+++ log/log.go
@@ -0,0 +1,65 @@
+package log
+
+import (
+	"log"
+	"os"
+)
+
+const(
+	LevelTrace = iota
+	LevelDebug
+	LevelInfo
+	LevelWarning
+	LevelError
+	LevelCritical
+)
+
+var level = LevelTrace
+
+func Level() int {
+	return level
+}
+
+func SetLevel(l int) {
+	level = l
+}
+
+var ChanuLogger  = log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+func SetLogger(l *log.Logger){
+	ChanuLogger = l
+}
+
+func Trace(v ...interface{}){
+	if level <= LevelTrace {
+		ChanuLogger.Printf("[T] %v¥n",v)
+	}
+}
+func Debug(v ...interface{}){
+	if level <= LevelDebug {
+		ChanuLogger.Printf("[T] %v¥n",v)
+	}
+}
+func Info(v ...interface{}){
+	if level <= LevelInfo {
+		ChanuLogger.Printf("[T] %v¥n",v)
+	}
+}
+func Warning(v ...interface{}){
+	if level <= LevelWarning {
+		ChanuLogger.Printf("[T] %v¥n",v)
+	}
+}
+func Error(v ...interface{}){
+	if level <= LevelError {
+		ChanuLogger.Printf("[T] %v¥n",v)
+	}
+}
+func Critical(v ...interface{}){
+	if level <= LevelCritical {
+		ChanuLogger.Printf("[T] %v¥n",v)
+	}
+}
+
+
+
--- main/main.go
+++ main/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"github.com/yuyutyanu/mvc"
 	"github.com/yuyutyanu/mvc/controllers"
+	log2 "github.com/yuyutyanu/mvc/log"
 	"log"
 	"net/http"
 )
@@ -10,10 +11,14 @@ import (
 
 
 func main() {
+	log2.SetLevel(log2.LevelInfo)
+	log2.Info("hogehoge")
+	log2.Debug("piyopiyo")
+
 	mvc.NewApp()
 	router := &mvc.ControllerRegsiter{}
 	router.Add("/user/:id/blog/:article", &controllers.RootController{})
-	err := http.ListenAndServe(":9090", router) //監視するポートを設定します。
+	err := http.ListenAndServe(":9090", router)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
