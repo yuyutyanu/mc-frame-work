2e7b0c9aff171321d56eedb3dba231798baff269
chanu <yuyutyanu@yahoo.co.jp>
2019-05-07 01:17:16 +0900

    add: config

--- app.go
+++ app.go
@@ -1,6 +1,9 @@
 package mvc
 
+import "github.com/yuyutyanu/mvc/conf"
+
 type App struct {
+	Cfg *conf.Config
 }
 
 var StaticDir = make(map[string]string)
@@ -13,6 +16,11 @@ func NewApp() *App{
 	app := new(App)
 	app.SetStaticPath("/img", "./static/img")
 	app.SetStaticPath("/css", "./static/css")
+	cfg, err := conf.LoadConfig(".env")
+	if err != nil{
+		panic(err)
+	}
+	app.Cfg = cfg
 	return app
 }
 
--- main/main.go
+++ main/main.go
@@ -1,25 +1,26 @@
 package main
 
 import (
+	"fmt"
 	"github.com/yuyutyanu/mvc"
-	"github.com/yuyutyanu/mvc/controllers"
-	log2 "github.com/yuyutyanu/mvc/log"
-	"log"
-	"net/http"
 )
 
-
-
 func main() {
-	log2.SetLevel(log2.LevelInfo)
-	log2.Info("hogehoge")
-	log2.Debug("piyopiyo")
-
-	mvc.NewApp()
-	router := &mvc.ControllerRegsiter{}
-	router.Add("/user/:id/blog/:article", &controllers.RootController{})
-	err := http.ListenAndServe(":9090", router)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	app := mvc.NewApp()
+	str := app.Cfg.String("DB_NAME")
+	fmt.Print(str)
+	for _, value := range app.Cfg.GetComment(){
+		fmt.Print(value)
 	}
+
+	//log2.SetLevel(log2.LevelInfo)
+	//log2.Info("hogehoge")
+	//log2.Debug("piyopiyo")
+
+	//router := &mvc.ControllerRegsiter{}
+	//router.Add("/user/:id/blog/:article", &controllers.RootController{})
+	//err := http.ListenAndServe(":9090", router)
+	//if err != nil {
+	//	log.Fatal("ListenAndServe: ", err)
+	//}
 }
